205430c205797a321eb807275cc75c9b398548dd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-02-21 19:14:36 -0600

    update

--- domain_script.ps1
+++ domain_script.ps1
@@ -2,8 +2,8 @@ Clear-Host
 Write-Host "`n`n`n`n`n`n"
 
 # Prompt the user for input
-$domain = Read-Host "Enter the domain name (e.g., example.com):"
-$port = Read-Host "Enter the desired port:"
+$domain = Read-Host "Enter the domain name (e.g., example.com)"
+$port = Read-Host "Enter the desired port"
 
 # Construct the command with the provided domain
 $cmd1 = "sudo mkdir -p /var/www/$domain/public_html"
@@ -26,12 +26,7 @@ $cmd4 = "echo 'Hello, world! Testing from $domain!' | sudo tee -a /var/www/$doma
 # Execute the command
 Invoke-Expression $cmd4
 
-$cmd5 = "sudo touch /etc/apache2/sites-available/$domain.conf"
-
-# Execute the command
-Invoke-Expression $cmd5
-
-$cmd6 = "echo '<VirtualHost *:$port>
+$cmd5 = "echo '<VirtualHost *:$port>
 ServerAdmin webmaster@localhost
 ServerName http://$domain.com 
 ServerAlias http://www.$domain.com 
@@ -41,27 +36,27 @@ CustomLog $%{APACHE_LOG_DIR}/access.log combined
 </VirtualHost>' | sudo tee -a /etc/apache2/sites-available/$domain.conf ; sudo sed -i 's/\%//g' /etc/apache2/sites-available/$domain.conf"
 
 # Execute the command
+Invoke-Expression $cmd5
+
+$cmd6 = "echo 'Listen $port' | sudo tee -a /etc/apache2/ports.conf"
+
+# Execute the command
 Invoke-Expression $cmd6
 
-$cmd7 = "echo 'Listen $port' | sudo tee -a /etc/apache2/ports.conf"
+$cmd7 = "sudo a2dissite 000-default.conf"
 
 # Execute the command
 Invoke-Expression $cmd7
 
-$cmd8 = "sudo a2dissite 000-default.conf"
+$cmd8 = "sudo a2ensite $domain.conf"
 
 # Execute the command
 Invoke-Expression $cmd8
 
-$cmd9 = "sudo a2ensite $domain.conf"
+$cmd9 = "sudo systemctl restart apache2"
 
 # Execute the command
 Invoke-Expression $cmd9
-
-$cmd10 = "sudo systemctl restart apache2"
-
-# Execute the command
-Invoke-Expression $cmd10
 
 
 Write-Host "Process completed successfully."
